be3c01fec780dd72010fbbfd42f722f8e6614a18
Haris Rotsos <cr409@cam.ac.uk>
2013-06-29 09:10:42 +0100

    fix by dimos. unistd.h include

--- ns3/lib/ns_stubs.cc
+++ ns3/lib/ns_stubs.cc
@@ -13,6 +13,8 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+ 
+#include <unistd.h>
 #include <arpa/inet.h>
 #include <netinet/tcp.h>
 
