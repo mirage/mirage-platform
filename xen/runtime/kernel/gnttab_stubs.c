f532fc79af41e0e39b624a8e63dffc900bf1b7e4
David Scott <dave.scott@eu.citrix.com>
2014-01-19 15:45:03 +0000

    Link to ocamlfind package 'xen-grant' for the Gnt module
    The same OCaml code can be used for both userspace and
    kernelspace.
    
    We keep the C stubs in-tree for now.
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- xen/runtime/kernel/gnttab_stubs.c
+++ xen/runtime/kernel/gnttab_stubs.c
@@ -31,18 +31,46 @@
 
 static grant_entry_t *gnttab_table;
 
-/* Return the size of the grant table */
-CAMLprim value
-caml_gnttab_nr_entries(value unit)
+CAMLprim value stub_gnttab_open(value unit)
 {
-    return Val_int(NR_GRANT_ENTRIES);
+	CAMLparam1(unit);
+	CAMLlocal1(result);
+	result = Val_unit;
+	CAMLreturn(result);
 }
 
-/* Return the number of reserved grant entries at the start */
-CAMLprim value
-caml_gnttab_reserved(value unit)
+CAMLprim value stub_gnttab_close(value unit)
 {
-    return Val_int(NR_RESERVED_ENTRIES);
+	CAMLparam1(unit);
+	CAMLlocal1(result);
+	result = Val_unit;
+	CAMLreturn(result);
+}
+
+CAMLprim value stub_gnttab_allocates(void)
+{
+	CAMLparam0();
+	CAMLreturn(Val_bool(0));
+}
+
+CAMLprim value
+caml_gnttab_unmap(value i, value v_handle)
+{
+  CAMLparam2(i, v_handle);
+  struct gnttab_unmap_grant_ref op;
+  /* There's no need to resupply these values. 0 means "ignore" */
+  op.host_addr = 0;
+  op.dev_bus_addr = 0;
+  op.handle = Int_val(v_handle);
+
+  HYPERVISOR_grant_table_op(GNTTABOP_unmap_grant_ref, &op, 1);
+
+  if (op.status != GNTST_okay) {
+    printk("GNTTABOP_unmap_grant_ref handle = %x failed", op.handle);
+    caml_failwith("Failed to unmap grant.");
+  }
+
+  CAMLreturn(Val_unit);
 }
 
 static void
@@ -52,6 +80,120 @@ gnttab_grant_access(grant_ref_t ref, void *page, int domid, int ro)
     gnttab_table[ref].domid = domid;
     wmb();
     gnttab_table[ref].flags = GTF_permit_access | (ro * GTF_readonly);
+}
+
+static void *
+base_page_of(value v_iopage)
+{
+    /* The grant API takes page-alignted addresses. */
+    struct caml_ba_array *a = (struct caml_ba_array *)Caml_ba_array_val(v_iopage);
+    unsigned long page_aligned_view = (unsigned long)a->data & ~(PAGE_SIZE - 1);
+    return (void*) page_aligned_view;
+}
+
+CAMLprim value
+caml_gnttab_map_onto(value i, value v_ref, value v_iopage, value v_domid, value v_writable)
+{
+    CAMLparam5(i, v_ref, v_iopage, v_domid, v_writable);
+    void *page = base_page_of(v_iopage);
+
+    struct gnttab_map_grant_ref op;
+    op.ref = Int_val(v_ref);
+    op.dom = Int_val(v_domid);
+    op.host_addr = (unsigned long) page;
+    op.flags = GNTMAP_host_map;
+    if (!Bool_val(v_writable)) op.flags |= GNTMAP_readonly;
+
+    HYPERVISOR_grant_table_op(GNTTABOP_map_grant_ref, &op, 1);
+    if (op.status != GNTST_okay) {
+      printk("GNTTABOP_map_grant_ref ref = %d domid = %d failed with status = %d\n", op.ref, op.dom, op.status);
+      caml_failwith("caml_gnttab_map");
+    }
+
+    printk("GNTTABOP_map_grant_ref mapped to %x\n", op.host_addr);
+    CAMLreturn(Val_int(op.handle));
+}
+
+CAMLprim value stub_gnttab_map_fresh(value i, value r, value d, value w)
+{
+    CAMLparam4(i, r, d, w);
+    /* The OCaml code will never call this because gnttab_allocates is false */
+    printk("FATAL ERROR: stub_gnttab_map_fresh called\n");
+    caml_failwith("stub_gnttab_map_fresh");
+}
+
+CAMLprim value stub_gnttab_mapv_batched(value xgh, value array, value writable)
+{
+    CAMLparam3(xgh, array, writable);
+    /* The OCaml code will never call this because gnttab_allocates is false */
+    printk("FATAL ERROR: stub_gnttab_mapv_batched called\n");
+    caml_failwith("stub_gnttab_mapv_batched");
+}
+
+
+/* Disable grant tables */
+CAMLprim value
+caml_gnttab_fini(value unit)
+{
+    struct gnttab_setup_table setup;
+
+    setup.dom = DOMID_SELF;
+    setup.nr_frames = 0;
+
+    HYPERVISOR_grant_table_op(GNTTABOP_setup_table, &setup, 1);
+
+    unmap_frames((unsigned long)gnttab_table, NR_GRANT_FRAMES);
+
+    return Val_unit;
+}
+
+/* Initialise grant tables and map machine frames to a VA */
+CAMLprim value
+caml_gnttab_init(value unit)
+{
+    struct gnttab_setup_table setup;
+    unsigned long frames[NR_GRANT_FRAMES];
+
+    setup.dom = DOMID_SELF;
+    setup.nr_frames = NR_GRANT_FRAMES;
+    set_xen_guest_handle(setup.frame_list, frames);
+
+    HYPERVISOR_grant_table_op(GNTTABOP_setup_table, &setup, 1);
+    gnttab_table = map_frames(frames, NR_GRANT_FRAMES);
+    printk("gnttab_table mapped at %p\n", gnttab_table);
+
+    return Val_unit;
+}
+
+/* Return the number of reserved grant entries at the start */
+CAMLprim value
+caml_gnttab_reserved(value unit)
+{
+    return Val_int(NR_RESERVED_ENTRIES);
+}
+
+CAMLprim value
+caml_gnttab_nr_entries(value unit)
+{
+    return Val_int(NR_GRANT_ENTRIES);
+}
+
+/* Exporting (sharing) pages */
+
+CAMLprim value stub_gntshr_open(value unit)
+{
+	CAMLparam1(unit);
+	CAMLlocal1(result);
+	result = Val_unit;
+	CAMLreturn(result);
+}
+
+CAMLprim value stub_gntshr_close(value unit)
+{
+	CAMLparam1(unit);
+	CAMLlocal1(result);
+	result = Val_unit;
+	CAMLreturn(result);
 }
 
 static void
@@ -93,69 +235,65 @@ gnttab_end_transfer(grant_ref_t ref)
     return frame;
 }
 
-static void *
-base_page_of(value v_iopage)
+static void
+gntshr_grant_access(grant_ref_t ref, void *page, int domid, int ro)
 {
-    /* The grant API takes page-alignted addresses. */
-    struct caml_ba_array *a = (struct caml_ba_array *)Caml_ba_array_val(v_iopage);
-    unsigned long page_aligned_view = (unsigned long)a->data & ~(PAGE_SIZE - 1);
-    return (void*) page_aligned_view;
+    gnttab_table[ref].frame = virt_to_mfn(page);
+    gnttab_table[ref].domid = domid;
+    wmb();
+    gnttab_table[ref].flags = GTF_permit_access | (ro * GTF_readonly);
 }
 
-CAMLprim value
-caml_gnttab_map(value v_ref, value v_iopage, value v_domid, value v_readonly)
+static void
+gntshr_grant_transfer(grant_ref_t ref, void *page, int domid)
 {
-    CAMLparam4(v_ref, v_iopage, v_domid, v_readonly);
-    void *page = base_page_of(v_iopage);
+    gnttab_table[ref].frame = virt_to_mfn(page);
+    gnttab_table[ref].domid = domid;
+    wmb();
+    gnttab_table[ref].flags = GTF_accept_transfer;
+}
 
-    struct gnttab_map_grant_ref op;
-    op.ref = Int_val(v_ref);
-    op.dom = Int_val(v_domid);
-    op.host_addr = (unsigned long) page;
-    op.flags = GNTMAP_host_map;
-    if (Bool_val(v_readonly)) op.flags |= GNTMAP_readonly;
+/* From the reference implementation
+   http://git.kernel.org/?p=linux/kernel/git/torvalds/linux.git;a=blob;f=drivers/xen/grant-table.c;hb=HEAD */
+static unsigned long
+gntshr_end_transfer(grant_ref_t ref)
+{
+    unsigned long frame;
+    uint16_t flags;
+    uint16_t *pflags = &gnttab_table[ref].flags;
 
-    HYPERVISOR_grant_table_op(GNTTABOP_map_grant_ref, &op, 1);
-    if (op.status != GNTST_okay) {
-      printk("GNTTABOP_map_grant_ref ref = %d domid = %d failed with status = %d\n", op.ref, op.dom, op.status);
-      caml_failwith("caml_gnttab_map");
+    /*
+     * If a transfer is not even yet started, try to reclaim the grant
+     * reference and return failure (== 0).
+     */
+    while (!((flags = *pflags) & GTF_transfer_committed)) {
+      if (synch_cmpxchg(pflags, flags, 0) == flags)
+        return 0;
     }
 
-    printk("GNTTABOP_map_grant_ref mapped to %x\n", op.host_addr);
-    CAMLreturn(Val_int(op.handle));
+    /* If a transfer is in progress then wait until it is completed. */
+    while (!(flags & GTF_transfer_completed)) {
+      flags = *pflags;
+    }
+
+    rmb();  /* Read the frame number /after/ reading completion status. */
+    frame = gnttab_table[ref].frame;
+    BUG_ON(frame == 0);
+
+    return frame;
 }
 
 CAMLprim value
-caml_gnttab_unmap(value v_handle)
-{
-  CAMLparam1(v_handle);
-  struct gnttab_unmap_grant_ref op;
-  /* There's no need to resupply these values. 0 means "ignore" */
-  op.host_addr = 0;
-  op.dev_bus_addr = 0;
-  op.handle = Int_val(v_handle);
-
-  HYPERVISOR_grant_table_op(GNTTABOP_unmap_grant_ref, &op, 1);
-
-  if (op.status != GNTST_okay) {
-    printk("GNTTABOP_unmap_grant_ref handle = %x failed", op.handle);
-    caml_failwith("Failed to unmap grant.");
-  }
-
-  CAMLreturn(Val_unit);
-}
-
-CAMLprim value
-caml_gnttab_grant_access(value v_ref, value v_iopage, value v_domid, value v_readonly)
+caml_gntshr_grant_access(value i, value v_ref, value v_iopage, value v_domid, value v_writable)
 {
     grant_ref_t ref = Int_val(v_ref);
     void *page = base_page_of(v_iopage);
-    gnttab_grant_access(ref, page, Int_val(v_domid), Bool_val(v_readonly));
+    gnttab_grant_access(ref, page, Int_val(v_domid), !Bool_val(v_writable));
     return Val_unit;
 }
 
 CAMLprim value
-caml_gnttab_end_access(value v_ref)
+caml_gnttab_end_access(value i, value v_ref)
 {
     grant_ref_t ref = Int_val(v_ref);
     uint16_t flags, nflags;
@@ -174,38 +312,16 @@ caml_gnttab_end_access(value v_ref)
     return Val_unit;
 }
 
-
-/* Initialise grant tables and map machine frames to a VA */
-CAMLprim value
-caml_gnttab_init(value unit)
-{
-    struct gnttab_setup_table setup;
-    unsigned long frames[NR_GRANT_FRAMES];
-
-    setup.dom = DOMID_SELF;
-    setup.nr_frames = NR_GRANT_FRAMES;
-    set_xen_guest_handle(setup.frame_list, frames);
-
-    HYPERVISOR_grant_table_op(GNTTABOP_setup_table, &setup, 1);
-    gnttab_table = map_frames(frames, NR_GRANT_FRAMES);
-    printk("gnttab_table mapped at %p\n", gnttab_table);
-
-    return Val_unit;
+CAMLprim value stub_gntshr_share_pages_batched(value xgh, value domid, value count, value writable) {
+    CAMLparam4(xgh, domid, count, writable);
+    /* The OCaml code will never call this because gnttab_allocates is false */
+    printk("FATAL ERROR: stub_gntshr_share_pages_batched called\n");
+    caml_failwith("stub_gntshr_share_pages_batched");
 }
 
-/* Disable grant tables */
-CAMLprim value
-caml_gnttab_fini(value unit)
-{
-    struct gnttab_setup_table setup;
-
-    setup.dom = DOMID_SELF;
-    setup.nr_frames = 0;
-
-    HYPERVISOR_grant_table_op(GNTTABOP_setup_table, &setup, 1);
-
-    unmap_frames((unsigned long)gnttab_table, NR_GRANT_FRAMES);
-
-    return Val_unit;
+CAMLprim value stub_gntshr_munmap_batched(value xgh, value share) {
+    CAMLparam2(xgh, share);
+    /* The OCaml code will never call this because gnttab_allocates is false */
+    printk("FATAL ERROR: stub_gntshr_munmap_batched called\n");
+    caml_failwith("stub_gntshr_munmap_batched");
 }
-
