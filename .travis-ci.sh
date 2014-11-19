36b32c178cf1f1ceeffc698a210a06bcb11ae687
Anil Madhavapeddy <anil@recoil.org>
2014-11-19 22:13:42 +0000

    fix travis to use opam 1.2.0 and correct opam repository branch

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -50,12 +50,12 @@ else
     setup_arm_chroot
   else
     case "$OCAML_VERSION,$OPAM_VERSION" in
-    3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-    3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-    4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
     4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
-    4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
+    4.00.1,1.2.0) ppa=avsm/ocaml40+opam12 ;;
     4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+    4.01.0,1.2.0) ppa=avsm/ocaml41+opam12 ;;
+    4.02.1,1.1.0) ppa=avsm/ocaml42+opam11 ;;
+    4.02.1,1.2.0) ppa=avsm/ocaml42+opam12 ;;
     *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
     esac
     echo "yes" | sudo add-apt-repository ppa:$ppa
@@ -71,7 +71,7 @@ echo OPAM versions
 opam --version
 opam --git-version
 
-opam init git://github.com/ocaml/opam-repository
+opam init
 
 opam install ${OPAM_PACKAGES}
 
