9b7999364b91238f18c97618512c7db0b22a55be
Anil Madhavapeddy <anil@recoil.org>
2015-01-11 23:09:04 +0000

    Install all the pkg-config packages from outside oasis

--- xen-ocaml/bindings/build.sh
+++ xen-ocaml/bindings/build.sh
@@ -1,13 +1,10 @@
 #!/bin/sh -ex
 
 export PKG_CONFIG_PATH=`opam config var prefix`/lib/pkgconfig
-PKG_CONFIG_DEPS="mirage-xen libminios-xen"
+PKG_CONFIG_DEPS="mirage-xen-minios"
 pkg-config --print-errors --exists ${PKG_CONFIG_DEPS} || exit 1
+CFLAGS=`pkg-config --cflags ${PKG_CONFIG_DEPS}`
 
-# TODO these need to be in the main minios flags exposed somehow
-EXTRA_MINIOS_CFLAGS="-D__XEN_INTERFACE_VERSION__=0x00030205 -D__INSIDE_MINIOS__"
 CC=${CC:-cc}
-for i in *.c; do
-  $CC -Wno-attributes ${EXTRA_MINIOS_CFLAGS} -c -Wall `pkg-config --cflags ${PKG_CONFIG_DEPS}` $i
-done
+$CC -Wno-attributes ${EXTRA_MINIOS_CFLAGS} -c -Wall ${CFLAGS} *.c
 ar rcs libxencamlbindings.a *.o
