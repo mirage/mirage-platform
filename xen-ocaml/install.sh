aa96b7c7deb31cb127703eb4787011ee85faed26
Thomas Leonard <talex5@gmail.com>
2015-06-29 17:20:10 +0100

    Cope with new header file location in OCaml 4.02.2

--- xen-ocaml/install.sh
+++ xen-ocaml/install.sh
@@ -18,12 +18,18 @@ cp mirage-xen-ocaml.pc $odir/pkgconfig/mirage-xen-ocaml.pc
 # Install public includes
 idir=$prefix/include/mirage-xen-ocaml/include
 mkdir -p $idir/caml
+
 cd ocaml-src/byterun
+if [ -f caml/alloc.h ]; then
+  HEADERS_SRC=caml
+else
+  HEADERS_SRC=.
+fi
 
 PUBLIC_INCLUDES="alloc.h callback.h config.h custom.h fail.h hash.h intext.h \
   memory.h misc.h mlvalues.h printexc.h signals.h compatibility.h"
 for i in ${PUBLIC_INCLUDES}; do
-  sed -f ../tools/cleanup-header $i > $idir/caml/$i
+  sed -f ../tools/cleanup-header $HEADERS_SRC/$i > $idir/caml/$i
 done
 cd ../otherlibs/bigarray
 sed -f ../../tools/cleanup-header bigarray.h > $idir/caml/bigarray.h
