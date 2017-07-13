8f79fcbe0d4e67e1ddf3106102741a9da6ea34dc
Anil Madhavapeddy <anil@recoil.org>
2017-07-13 18:08:38 +0100

    support 4.04.2 and 4.05.0
    this patcheset simplifies and eliminates older patches in order
    to bring a bit more sanity to the build.  It no longer defines
    m.h and runs the configure script on the host instead, and removes
    older patches for 4.01/2/3 in the interests of supporting two
    active versions.

--- xen-ocaml/config/s.h
+++ xen-ocaml/config/s.h
@@ -1,44 +1,57 @@
-#define OCAML_OS_TYPE "Unix"
+#define OCAML_OS_TYPE "xen"
 #define OCAML_STDLIB_DIR "/usr/local/lib/ocaml"
-#define POSIX_SIGNALS
-//#define HAS_GETRUSAGE
+#define  POSIX_SIGNALS
+#define HAS_C99_FLOAT_OPS
+#define HAS_GETRUSAGE
 #define HAS_TIMES
-//#define HAS_SOCKETS
-//#define HAS_SOCKLEN_T
-//#define HAS_INET_ATON
-//#define HAS_IPV6
-//#define HAS_UNISTD
-//#define HAS_OFF_T
+#undef HAS_ISSETUGID
+#undef HAS_TERMCAP
+#undef HAS_SOCKETS
+#undef HAS_SOCKLEN_T
+#define HAS_INET_ATON
+#define HAS_IPV6
+#define HAS_STDINT_H
+#define HAS_UNISTD
+#define HAS_OFF_T
 #define HAS_DIRENT
-//#define HAS_REWINDDIR
-//#define HAS_LOCKF
-//#define HAS_MKFIFO
+#define HAS_REWINDDIR
+#define HAS_LOCKF
+#define HAS_MKFIFO
 #define HAS_GETCWD
-#define HAS_EXPM1_LOG1P
-//#define HAS_GETWD
-//#define HAS_GETPRIORITY
-//#define HAS_UTIME
-//#define HAS_UTIMES
-//#define HAS_DUP2
-//#define HAS_FCHMOD
-//#define HAS_TRUNCATE
-//#define HAS_SYS_SELECT_H
-//#define HAS_SELECT
-//#define HAS_SYMLINK
-//#define HAS_WAITPID
-//#define HAS_WAIT4
-//#define HAS_GETGROUPS
-//#define HAS_TERMIOS
-//#define HAS_SETITIMER
-//#define HAS_GETHOSTNAME
-//#define HAS_UNAME
+#define HAS_GETPRIORITY
+#define HAS_UTIME
+#define HAS_UTIMES
+#define HAS_DUP2
+#define HAS_FCHMOD
+#define HAS_TRUNCATE
+#define HAS_SYS_SELECT_H
+#define HAS_SELECT
+#define HAS_NANOSLEEP
+#define HAS_SYMLINK
+#define HAS_WAITPID
+#define HAS_WAIT4
+#define HAS_GETGROUPS
+#define HAS_SETGROUPS
+#define HAS_INITGROUPS
+#define HAS_TERMIOS
+#define HAS_ASYNC_IO
+#define HAS_SETITIMER
+#define HAS_GETHOSTNAME
+#define HAS_UNAME
 #define HAS_GETTIMEOFDAY
-//#define HAS_MKTIME
-//#define HAS_SETSID
-//#define HAS_PUTENV
-//#define HAS_LOCALE
-//#define HAS_MMAP
-//#define HAS_GETHOSTBYNAME_R 6
-//#define HAS_GETHOSTBYADDR_R 8
-//#define HAS_STACK_OVERFLOW_DETECTION
-#define PROFINFO_WIDTH 26
+#define HAS_MKTIME
+#define HAS_SETSID
+#define HAS_PUTENV
+#undef HAS_LOCALE
+#undef SUPPORT_DYNAMIC_LINKING
+#undef HAS_MMAP
+#undef HAS_PWRITE
+#define HAS_NANOSECOND_STAT 1
+#define HAS_GETHOSTBYNAME_R 6
+#define HAS_GETHOSTBYADDR_R 8
+#define HAS_MKSTEMP
+#define HAS_NICE
+#undef HAS_STACK_OVERFLOW_DETECTION
+#undef HAS_SIGWAIT
+#undef HAS_HUGE_PAGES
+//#define HUGE_PAGE_SIZE (4 * 1024 * 1024)
