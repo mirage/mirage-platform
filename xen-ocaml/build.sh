82eaa3c8721a2a3246d16ef95d127b8cdc6e4a6f
Kate <kit.ty.kate@disroot.org>
2020-06-10 00:35:27 +0100

    OCaml 4.11 support

--- xen-ocaml/build.sh
+++ xen-ocaml/build.sh
@@ -125,6 +125,14 @@ case $OCAMLOPT_VERSION in
   CFLAGS="-D__ANDROID__ $CFLAGS"
   CONFIGURE_OPTS="--disable-systhreads --disable-str-lib --disable-unix-lib --disable-ocamldoc"
   ;;
+4.11.*)
+  echo Applying OCaml 4.11.0 config
+  cp config/version-4110.h ocaml-src/runtime/caml/version.h
+  S_H_LOCATION="ocaml-src/runtime/caml/"
+  BIGARRAY_OBJ=""
+  CFLAGS="-D__ANDROID__ $CFLAGS"
+  CONFIGURE_OPTS="--disable-systhreads --disable-str-lib --disable-unix-lib --disable-ocamldoc"
+  ;;
 *)
   echo unsupported OCaml version $OCAMLOPT_VERSION
   exit 1
@@ -145,7 +153,7 @@ case $OCAMLOPT_VERSION in
   cd asmrun && make -j${NJOBS} UNIX_OR_WIN32=unix CPPFLAGS="-DNATIVE_CODE ${CFLAGS} -I../byterun -DTARGET_${TARGET_ARCH}" NATIVECCPROFOPTS="-DNATIVE_CODE ${CFLAGS}" libasmrun.a && cd ..
   IS_408_OR_MORE=0
   ;;
-4.08.*|4.09.*|4.10.*)
+4.08.*|4.09.*|4.10.*|4.11.*)
   cd runtime && make -j${NJOBS} UNIX_OR_WIN32=unix OC_NATIVE_CPPFLAGS="-DNATIVE_CODE -DXXXX=1 ${CFLAGS} -DTARGET_${TARGET_ARCH}" libasmrun.a && cd ..
   IS_408_OR_MORE=1
   ;;
